30caec9eac087f38fd30a98e6c01dc4045e0ac78
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2022-08-30 19:29:48 +0300

    Spark 3.2.1 (#2)
    spark-3.2.1

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Spark for the Cloud Pipeline
 
-[![Build Status](https://travis-ci.org/sidoruka/cloud-pipeline-spark-build.svg?branch=master)](https://travis-ci.org/sidoruka/cloud-pipeline-spark-build)
-
 Builds a Spark distrubution for the [EPAM/cloud-pipeline](https://github.com/epam/cloud-pipeline)
-* Spark 2.4.3
+* Spark 3.2.1
 * Hadoop 3.1.0
 * Cloud Hadoop
-* Hive 1.2.1.spark2
 
-Distribution is uploaded to the S3 bucket: [s3://cloud-pipeline-oss-builds/tools/spark/spark-2.4.3.tgz](https://cloud-pipeline-oss-builds.s3.amazonaws.com/tools/spark/spark-2.4.3.tgz)
+Distribution is uploaded to the S3 bucket: [s3://cloud-pipeline-oss-builds/tools/spark/spark-3.2.1.tgz](https://cloud-pipeline-oss-builds.s3.amazonaws.com/tools/spark/spark-3.2.1.tgz)
--- build_spark.sh
+++ build_spark.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+### Setup prerequisites
+apt install git curl -y
+pip install awscli
+
+
 ### Setup JAVA 8
 echo "============================"
 echo "Setting up OpenJDK 1.8.0_222"
@@ -35,38 +40,26 @@ mkdir -p ~/credentials-provider/dist && \
 echo
 
 
-echo "============="
-echo "Building hive"
-echo "============="
-### See https://github.com/apache/spark/pull/20923 and https://github.com/JoshRosen/hive/pull/2 for the hive rebuild reasons
-rm -rf ~/hive && \
-cd ~ && \
-git clone https://github.com/sidoruka/hive.git && \
-cd hive && \
-git checkout release-1.2.1-spark2 && \
-mvn clean install -Phadoop-2 -DskipTests -Psources -q --batch-mode && \
-rm -rf dist && \
-mkdir dist && \
-\cp ~/.m2/repository/org/spark-project/hive/hive-beeline/1.2.1.spark2/hive-beeline-1.2.1.spark2.jar \
-    ~/.m2/repository/org/spark-project/hive/hive-cli/1.2.1.spark2/hive-cli-1.2.1.spark2.jar \
-    ~/.m2/repository/org/spark-project/hive/hive-exec/1.2.1.spark2/hive-exec-1.2.1.spark2.jar \
-    ~/.m2/repository/org/spark-project/hive/hive-jdbc/1.2.1.spark2/hive-jdbc-1.2.1.spark2.jar \
-    ~/.m2/repository/org/spark-project/hive/hive-metastore/1.2.1.spark2/hive-metastore-1.2.1.spark2.jar \
-    dist/
-echo
-
-
 echo "=============="
 echo "Building Spark"
 echo "=============="
-### Build Spark with Hadoop 3.1.0 and "Cloud Integration" (https://spark.apache.org/docs/2.4.3/cloud-integration.html)
+### If this is not set, Maven will crash with StackOverflow exception
+export MAVEN_OPTS="-Xss64m -Xmx2g -XX:ReservedCodeCacheSize=1g"
+### Build Spark with Hadoop 3.1.0 and "Cloud Integration" (https://spark.apache.org/docs/3.2.1/cloud-integration.html)
 rm -rf ~/spark && \
 cd ~ && \
 git clone https://github.com/apache/spark.git && \
 cd spark && \
-git checkout tags/v2.4.3 && \
+git checkout tags/v3.2.1 && \
 rm -rf dist && \
-./dev/make-distribution.sh --name cloud-pipeline-spark --tgz -Phadoop-cloud -Dhadoop.version=3.1.0 -Phive -DskipTests --batch-mode
+./dev/make-distribution.sh  --name cloud-pipeline-spark \
+                            --tgz \
+                            -Phadoop-cloud \
+                            -Dhadoop.version=3.2.2 \
+                            -Phive \
+                            -Phive-thriftserver \
+                            -DskipTests \
+                            --batch-mode
 echo
 
 
@@ -78,17 +71,14 @@ echo "===================="
 ### Add credentials provider
 \cp ~/credentials-provider/dist/* ~/spark/dist/jars/
 # Pack tar.gz
-mv ~/spark/dist ~/spark/spark-2.4.3-bin-hadoop3.1 && \
+mv ~/spark/dist ~/spark/spark-3.2.1-bin-hadoop3.2 && \
 cd ~/spark && \
-tar -zcf spark-2.4.3.tgz spark-2.4.3-bin-hadoop3.1
+tar -zcf spark-3.2.1.tgz spark-3.2.1-bin-hadoop3.2
 echo
 
-if ([ "$TRAVIS_BRANCH" == "master" ] || [[ "$TRAVIS_BRANCH" == "release/"* ]]) && \
-    ([ "$TRAVIS_EVENT_TYPE" == "push" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]); then
-        echo "======================="
-        echo "Publishing Spark distro"
-        echo "======================="
-        ### Upload to the distro-S3
-        aws s3 cp spark-2.4.3.tgz s3://cloud-pipeline-oss-builds/tools/spark/spark-2.4.3.tgz
-        echo
-fi
+echo "======================="
+echo "Publishing Spark distro"
+echo "======================="
+### Upload to the distro-S3
+aws s3 cp spark-3.2.1.tgz s3://cloud-pipeline-oss-builds/tools/spark/spark-3.2.1.tgz
+echo
